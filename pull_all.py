e38823ff22c45f703cda2dc6b30f7d7d9e1cdf12
Odd Stråbø <oddstr13@openshell.no>
2021-03-28 20:13:54 +0200

    Updated pull_all.py

--- pull_all.py
+++ pull_all.py
@@ -5,62 +5,66 @@ import requests
 import time
 
 
+try:
+    subprocess.run(['git', 'diff', '--staged', '--quiet', '--exit-code'], check=True)
+except subprocess.SubprocessError:
+    print("Error: You have staged changes!")
+    exit(1)
+
 #print(repos)
 available = []
 fetched = []
+failed = []
+removed = []
+added = []
 
 def update(_name, url=None):
-    print('-----')
-    print(_name)
-    print(description)
-    print(url)
-
     if not os.path.exists(_name):
-        subprocess.run(['git', 'clone', url, _name], check=True)
-        subprocess.run(['git', 'remote', 'rename', 'origin', 'upstream'], cwd=_name, check=True)
+        print("Adding {} @ {}".format(_name, url))
+        subprocess.run(['git', 'submodule', 'add', '-b', 'master', url, _name], check=True)
+        added.append(_name)
+    subprocess.run(['git', 'submodule', 'update', '--init', '--remote', '--checkout', '--force', _name], check=True)
 
-    subprocess.run(['git', 'checkout', '-f', '**/*.csproj', '*.csproj'], cwd=_name, check=False)
-    subprocess.run(['git', 'checkout', '-f', 'build.yaml'], cwd=_name, check=False)
-    subprocess.run(['git', 'checkout', 'master'], cwd=_name, check=True)
     subprocess.run(['git', 'fetch', '--all', '--tags'], cwd=_name, check=True)
-    subprocess.run(['git', 'pull', 'upstream', 'master', '--ff-only'], cwd=_name, check=True)
+    subprocess.run(['git', 'checkout', '-f', '-B', 'master', 'origin/master'], cwd=_name, check=True)
     fetched.append(_name)
 
-failed = []
+
+def remove(_name):
+    if os.path.exists(_name):
+        print("Removing {}".format(_name))
+        subprocess.run(['git', 'rm', _name], check=True)
+        removed.append(_name)
+
+
 
 page_num = 1
 per_page = 100
 PAGINATION_URL = 'https://api.github.com/orgs/jellyfin/repos?sort=created&per_page={per}&page={page}'
+
 next = PAGINATION_URL.format(per=per_page, page=page_num)
+
 while next:
     resp = requests.get(next)
-    
-    page_num += 1
-    next = PAGINATION_URL.format(per=per_page, page=page_num)
-    #next = None  # TODO: Add pagination support
-
     repos = resp.json()
 
+    page_num += 1
+    next = PAGINATION_URL.format(per=per_page, page=page_num)
     if len(repos) < per_page:
         next = None
 
-
     for repo in repos:
         _name = repo.get('name')
-        description = repo.get('description')
         url = repo.get('clone_url')
         if _name.startswith('jellyfin-plugin-'):
+            available.append(_name)
             try:
-                available.append(_name)
-                #update(_name, url)
+                update(_name, url)
                 pass
             except Exception as e:
                 failed.append((_name, e))
 
-for repo in available:
-    if not os.path.exists(repo):
-        print("Available:", repo)
-#exit()
+
 for repo in os.listdir('.'):
     if repo in fetched:
         continue
@@ -70,10 +74,10 @@ for repo in os.listdir('.'):
 
     if not repo.startswith('jellyfin-plugin-'):
         continue
+
     try:
         if not repo in available:
-            print("Not available:", repo)
-        #update(repo)
+            remove(repo)
     except Exception as e:
         failed.append((repo, e))
 
@@ -82,3 +86,18 @@ if failed:
     print("The following repositories failed to update:")
     for repo, e in failed:
         print(repo, e)
+
+commit_message = ["Updated plugin submodules"]
+
+if added:
+    commit_message.append('Added:')
+    for plugin in added:
+        commit_message.append("- {}".format(plugin))
+    commit_message.append('')
+
+if removed:
+    commit_message.append('Removed:')
+    for plugin in removed:
+        commit_message.append("- {}".format(plugin))
+
+subprocess.run(["git", 'commit', '-m', commit_message])
