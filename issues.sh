f2c9ae03ee8247ca7d07ebcb9d546f142167512b
Odd Stråbø <oddstr13@openshell.no>
2021-03-28 20:32:09 +0200

    Update issues and pullrequests

--- issues.sh
+++ issues.sh
@@ -1,13 +1,4 @@
-for plugin in $(find . -maxdepth 1 -mindepth 1 -type d -name 'jellyfin-plugin-*'); do
-  pushd $plugin > /dev/null
-    data=$(hub issue --format='%sC%>(8)%i%Creset %t% l%n%Cblue%         U%Creset%n' --color=always)
+#!/bin/bash
 
-    if [ ! -z "$data" ]; then
-      basename $plugin
-      echo "${data}"
-      echo
-    fi
-
-  popd > /dev/null
-done
-
+git submodule foreach \
+  hub issue --format='%sC%>(8)%i%Creset %t% l%n%Cblue%         U%Creset%n' --color=always
--- pullrequests.sh
+++ pullrequests.sh
@@ -1,15 +1,4 @@
+#!/bin/bash
 
-
-for plugin in $(find . -maxdepth 1 -mindepth 1 -type d -name 'jellyfin-plugin-*'); do
-  pushd $plugin > /dev/null
-    data=$(hub pr list --format='%pC%>(8)%i%Creset %t% l%n%Cblue%         U%Creset%n' --color=always)
-
-    if [ ! -z "$data" ]; then
-      basename $plugin
-      echo "${data}"
-      echo
-    fi
-
-  popd > /dev/null
-done
-
+git submodule foreach \
+  hub pr list --format='%pC%>(8)%i%Creset %t% l%n%Cblue%         U%Creset%n' --color=always
